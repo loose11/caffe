977f6c7a6957f02eb51c1aafd1ee21a69a4bf3be
Matthias Lieb <matthias.lieb89@gmail.com>
2016-10-12 17:49:24 +0200

    lines fix

--- src/caffe/layers/image_data_layer.cpp
+++ src/caffe/layers/image_data_layer.cpp
@@ -156,7 +156,7 @@ void ImageDataLayer<Dtype>::load_batch(Batch<Dtype>* batch) {
   int box_position = 0;
   for (int item_id = 0; item_id < batch_size; ++item_id) {
 
-    if(lines_id_ > 0 && lines_[lines_id_].first == lines_[lines_id_-1].first){
+    if(lines_id_+1 < lines_.size() && lines_[lines_id_].first == lines_[lines_id_+1].first){
       box_position++;
     }else{
       box_position = 0;
