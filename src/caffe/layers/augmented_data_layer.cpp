425c2e1c611b12ea226db269f22544b25a5b04b7
Matthias <matthias.lieb89@gmail.com>
2016-10-26 17:21:54 +0200

    fix

--- src/caffe/layers/augmented_data_layer.cpp
+++ src/caffe/layers/augmented_data_layer.cpp
@@ -196,7 +196,7 @@ void AugmentedDataLayer<Dtype>::load_batch(Batch<Dtype>* batch) {
     // Comment in if you want to save the images
     if(!output_directory.empty()){
       char buffer[300];
-      sprintf(buffer, "%s%s_%d_%d.png", *output_directory, create_raw_name(lines_[lines_id_].first).c_str(), lines_[lines_id_].second, item_id);
+      sprintf(buffer, "%s%s_%d_%d.png", &output_directory, create_raw_name(lines_[lines_id_].first).c_str(), lines_[lines_id_].second, item_id);
       std::string path = buffer;
       cv::imwrite(path, resize_image(augmented_images.at(0), new_width, new_height));
     }
