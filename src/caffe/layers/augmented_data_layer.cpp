0bf03f204b508cfce4998ebeb39d9aaaaab2d530
Matthias <matthias.lieb89@gmail.com>
2016-11-01 12:06:04 +0100

    fix

--- src/caffe/layers/augmented_data_layer.cpp
+++ src/caffe/layers/augmented_data_layer.cpp
@@ -53,7 +53,7 @@ void AugmentedDataLayer<Dtype>::DataLayerSetUp(const vector<Blob<Dtype>*>& botto
   const float s_deviation = aug_data_param.deviation();
   const float mean = aug_data_param.mean();
 
-  CHECK(s_deviation >= 0) << "Deviation could not be smaller than 0."
+  CHECK(s_deviation >= 0) << "Deviation could not be smaller than 0.";
 
   CHECK((new_height == 0 && new_width == 0) ||
       (new_height > 0 && new_width > 0)) << "Current implementation requires "
